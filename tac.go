b8712bb614ed5018cc27c1367655c1927ddfb0dd
Sebastien BENOIT <sb@42.fr>
2018-06-08 15:28:36 +0200

    getuserbytag and json decode ok

--- tac.go
+++ tac.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
+	"regexp"
 )
 
 type Tac struct {
@@ -13,7 +16,18 @@ type Tac struct {
 	login    string
 	passwd   string
 	cookie   http.Cookie
+	jar      *cookiejar.Jar
 	loggedOn bool
+}
+
+type TacUserProfil struct {
+	ID       string      `json:"id"`
+	Name     string      `json:"name"`
+	Company  string      `json:"company"`
+	Valid    interface{} `json:"valid"`
+	Expired  interface{} `json:"expired"`
+	Disabled string      `json:"disabled"`
+	Apbypass string      `json:"apbypass"`
 }
 
 func (t *Tac) SetCredentials(tac_url, login, passwd string) {
@@ -22,12 +36,17 @@ func (t *Tac) SetCredentials(tac_url, login, passwd string) {
 	t.login = login
 	t.passwd = passwd
 	t.loggedOn = false
+	t.jar, _ = cookiejar.New(nil)
 	fmt.Printf("url: %s...\n", t.url)
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 }
 
 func (t *Tac) Request(action string, params []string) (code int, body string) {
 	fmt.Println("%s: %#v", action, params)
+
+	client := &http.Client{
+		Jar: t.jar,
+	}
 
 	v := url.Values{}
 	v.Set("rpc[func]", action)
@@ -38,7 +57,7 @@ func (t *Tac) Request(action string, params []string) (code int, body string) {
 		i++
 	}
 	fmt.Println("v: %#v", v)
-	resp, err := http.PostForm(t.url+"action.php", v)
+	resp, err := client.PostForm(t.url+"action.php", v)
 	if err != nil {
 		fmt.Printf("%s failed: %s\n", action, err.Error())
 		return -1, ""
@@ -71,19 +90,17 @@ func (t *Tac) Login() {
 
 	fmt.Printf("login in...\n")
 	fmt.Printf("get %s\n", t.url)
-	resp, err := http.Get(t.url)
+
+	client := &http.Client{
+		Jar: t.jar,
+	}
+	resp, err := client.Get(t.url)
 	if err != nil {
 		fmt.Printf("Login 1 failed: %s\n", err.Error())
 		return
 	}
-	if len(resp.Cookies()) < 1 {
-		fmt.Printf("No cookie\n")
-		return
-	}
-	t.cookie = *resp.Cookies()[0]
-	fmt.Printf("%s\n", t.cookie.String())
 
-	fmt.Printf("login on %s with %s:%s creds\n", t.url, t.login, t.passwd)
+	fmt.Printf("login on %s with %s:%s creds: %s\n", t.url, t.login, t.passwd, resp.Status)
 
 	v := url.Values{}
 	v.Set("rpc[func]", "taction_login")
@@ -93,7 +110,7 @@ func (t *Tac) Login() {
 
 	fmt.Println("%#v", v)
 
-	resp2, err2 := http.PostForm(t.url+"action.php", v)
+	resp2, err2 := client.PostForm(t.url+"action.php", v)
 	if err2 != nil {
 		fmt.Printf("Login 2 failed: %s\n", err2.Error())
 		return
@@ -103,6 +120,23 @@ func (t *Tac) Login() {
 	return
 }
 
+func (t *Tac) ParseResponse(body string) string {
+	r, _ := regexp.Compile(`\[(.*),\s+\]`)
+	res := r.FindStringSubmatch(body)
+	for index, match := range res {
+		fmt.Printf("[%d] %s\n", index, match)
+	}
+	return res[1]
+}
+
 func (t *Tac) GetUserByTag(tag string) (code int, body string) {
-	return t.Request("taction_get_user_bytag", []string{t.ReverseTag(tag)})
+	var p TacUserProfil
+	code, body = t.Request("taction_get_user_bytag", []string{t.ReverseTag(tag)})
+	res := t.ParseResponse(body)
+	err := json.Unmarshal([]byte(res), &p)
+	if err != nil {
+		fmt.Println("json decode error: %s", err.Error())
+	}
+	fmt.Println("%#v", p)
+	return code, res
 }
